10e232a659ccad3cc909c57dc51a9a6c858281a9
edmine-cpu <sorokinonetwofour3@gmail.com>
2026-06-12 11:50:31 +0200

    include main router

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher
 from settings import settings
+from handlers import main_router
 
 
 dp = Dispatcher()
@@ -8,4 +9,5 @@ token = settings.BOT_TOKEN
 
 async def main():
 	bot = Bot(token=token)
+	dp.include_router(main_router)
 	dp.start_polling(bot)
