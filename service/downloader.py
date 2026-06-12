b3a6d2507b8a85f09baada773b2adb877028b7f0
edmine-cpu <sorokinonetwofour3@gmail.com>
2026-06-12 13:48:46 +0200

    theading + readme complete

--- service/downloader.py
+++ service/downloader.py
@@ -1,3 +1,4 @@
+import asyncio
 import validators
 from aiogram.types import Message, FSInputFile
 from utils.temp_video import TempVideo
@@ -6,16 +7,19 @@ from handlers.constants.messages import messages
 
 
 async def download_video(url: str, message: Message):
-	if validators.url(url):
-		downloaded_video = download_video_util(url)
-		async with TempVideo(downloaded_video.path) as path:
-			video = FSInputFile(path)
-			await message.answer_video(
-				video,
-				duration=downloaded_video.duration,
-				width=downloaded_video.width,
-				height=downloaded_video.height,
-				supports_streaming=True,
-			)
-	else:
+	if not validators.url(url):
 		await message.answer(messages["VALIDATION_ERROR"])
+		return
+	
+
+	downloaded_video = asyncio.to_thread(download_video_util, url)
+	async with TempVideo(downloaded_video.path) as path:
+		video = FSInputFile(path)
+		await message.answer_video(
+			video,
+			duration=downloaded_video.duration,
+			width=downloaded_video.width,
+			height=downloaded_video.height,
+			supports_streaming=True,
+		)
+		
