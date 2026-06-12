9f77cd892236e5b8960cb99b9275cd731328116f
edmine-cpu <sorokinonetwofour3@gmail.com>
2026-06-12 12:21:55 +0200

    url download handler

---
+++ handlers/url.py
@@ -0,0 +1,12 @@
+from aiogram import Router
+from aiogram.types import Message
+from .constants.messages import messages
+from service import downloader
+
+
+router = Router()
+
+
+@router.message()
+async def download_by_url(message: Message):
+	await downloader.download_video(message.text, message)
