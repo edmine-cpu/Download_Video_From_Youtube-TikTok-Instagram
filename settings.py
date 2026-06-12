8e76c5aa1ad22837b89c5207b957cc6e0da1a6d7
edmine-cpu <sorokinonetwofour3@gmail.com>
2026-06-12 11:40:03 +0200

    add export 'settings' variable

--- settings.py
+++ settings.py
@@ -4,5 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
 	BOT_TOKEN: str
 
-
 	model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
