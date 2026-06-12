223c3bbe8e0a2b3e4705933895035b93e3add52a
edmine-cpu <sorokinonetwofour3@gmail.com>
2026-06-12 12:34:07 +0200

    include router

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,8 @@
 from aiogram import Router
 from .start import router as start_router
+from .url import router as download_router
 
 
-routers = [start_router]
+routers = [start_router, download_router]
 main_router = Router()
 main_router.include_routers(*routers)
