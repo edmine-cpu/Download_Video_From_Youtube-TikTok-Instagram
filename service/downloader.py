2e80cc15657a11cdd4753c7ab058edc1043faa60
edmine-cpu <sorokinonetwofour3@gmail.com>
2026-06-12 12:33:54 +0200

    add validation url

--- service/downloader.py
+++ service/downloader.py
@@ -1,11 +1,15 @@
+import validators
 from aiogram.types import Message, FSInputFile
 from utils.temp_video import TempVideo
 from utils.downloader import download_video as download_video_util
-
+from handlers.constants.messages import messages
 
 
 async def download_video(url: str, message: Message):
-	path = download_video_util(url)
-	async with TempVideo(path) as path:
-		video = FSInputFile(path)
-		await message.answer_video(video)
+	if validators.url(url):
+		path = download_video_util(url)
+		async with TempVideo(path) as path:
+			video = FSInputFile(path)
+			await message.answer_video(video)
+	else:
+		await message.answer(messages["VALIDATION_ERROR"])
