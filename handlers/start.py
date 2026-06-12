c3ff95891d60a16aee027e9c72e7c46f0829b7fd
edmine-cpu <sorokinonetwofour3@gmail.com>
2026-06-12 11:49:59 +0200

    start handler

---
+++ handlers/start.py
@@ -0,0 +1,12 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from constants import messages
+
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def start(message: Message):
+	await message.answer(messages["START"])
