3dce730f8afe5be3ef95a66d8d6e4386e07d8f20
edmine-cpu <sorokinonetwofour3@gmail.com>
2026-06-12 13:59:47 +0200

    constants update

--- handlers/constants/messages.py
+++ handlers/constants/messages.py
@@ -1,4 +1,4 @@
 messages = {
 	"START": "Привет, я телеграм бот для скачивания видео из Youtube/TikTok/Instagram\nТебе нужно просто отправить ссылку на видео.",
-	"VALIDATION_ERROR": "Это не ссылка."
+	"VALIDATION_ERROR": "Ошибка, это не ссылка."
 }
